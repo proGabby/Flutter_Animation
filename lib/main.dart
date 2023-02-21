a892159571eb80e0cfe17d79ae4f0ad8aabbf2b2
proGabby <inimfonwillie@gmail.com>
2023-02-21 15:42:19 +0100

    draggable_widget

--- lib/main.dart
+++ lib/main.dart
@@ -16,198 +16,56 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const RotatingAndFlippingCircle(),
+      home: FloatingWidget(),
     );
   }
 }
 
-enum CircleSide {
-  left,
-  right,
+class FloatingWidget extends StatefulWidget {
+  @override
+  _FloatingWidgetState createState() => _FloatingWidgetState();
 }
 
-extension ToPath on CircleSide {
-  Path toPath(Size size) {
-    var path = Path();
-    late Offset offset;
-    late bool clockwise;
-
-    switch (this) {
-      case CircleSide.left:
-        path.moveTo(size.width, 0);
-        offset = Offset(size.width, size.height);
-        clockwise = false;
-        break;
-      case CircleSide.right:
-        offset = Offset(0, size.height);
-        clockwise = true;
-        break;
-      default:
-    }
-    path.arcToPoint(offset,
-        radius: Radius.elliptical(size.width / 2, size.height / 2),
-        clockwise: clockwise);
-    path.close();
-    return path;
-  }
-}
-
-class HalfCircleClipper extends CustomClipper<Path> {
-  final CircleSide circeSide;
-
-  HalfCircleClipper(this.circeSide);
-
-  @override
-  Path getClip(Size size) {
-    return circeSide.toPath(size);
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
-  }
-}
-
-class RotatingAndFlippingCircle extends StatefulWidget {
-  const RotatingAndFlippingCircle({
-    super.key,
-  });
-
-  @override
-  State<RotatingAndFlippingCircle> createState() =>
-      _MyRotatingAndFlippingCircle();
-}
-
-class _MyRotatingAndFlippingCircle extends State<RotatingAndFlippingCircle>
-    with TickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  late AnimationController _flipController;
-  late Animation<double> _flipAnimation;
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    _animation = Tween<double>(begin: 0.0, end: -(pi / 2))
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.bounceOut));
-
-    _controller.forward();
-
-    _flipController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-
-    _flipAnimation = Tween<double>(
-      begin: 0,
-      end: pi,
-    ).animate(
-      CurvedAnimation(parent: _flipController, curve: Curves.bounceInOut),
-    );
-
-    _controller.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        _flipAnimation = Tween<double>(
-          begin: _flipAnimation.value,
-          end: _flipAnimation.value + pi,
-        ).animate(
-          CurvedAnimation(parent: _flipController, curve: Curves.bounceInOut),
-        );
-
-        _flipController
-          ..reset()
-          ..forward();
-      }
-    });
-
-    _flipController.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        _animation = Tween<double>(
-                begin: _animation.value, end: _animation.value + -(pi / 2))
-            .animate(
-                CurvedAnimation(parent: _controller, curve: Curves.bounceOut));
-        _controller
-          ..reset()
-          ..forward();
-      }
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    _flipController.dispose();
-    super.dispose();
-  }
+class _FloatingWidgetState extends State<FloatingWidget> {
+  double _x = 0.0;
+  double _y = 0.0;
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(Duration(seconds: 1), () {
-      _controller
-        ..reset()
-        ..forward();
-    });
     return Scaffold(
-      body: SafeArea(
-        child: AnimatedBuilder(
-          animation: _controller,
-          builder: (context, animeChild) {
-            return Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.identity()..rotateZ(_animation.value),
-              child: Center(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    AnimatedBuilder(
-                      animation: _flipController,
-                      builder: (context, child) {
-                        return Transform(
-                          alignment: Alignment.centerRight,
-                          transform: Matrix4.identity()
-                            ..rotateY(_flipAnimation.value),
-                          child: ClipPath(
-                            clipper: HalfCircleClipper(CircleSide.left),
-                            child: Container(
-                              height: 100,
-                              width: 100,
-                              color: Colors.grey,
-                            ),
-                          ),
-                        );
-                      },
+      body: Stack(
+        children: [
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 100),
+            left: _x,
+            top: _y,
+            child: GestureDetector(
+              onPanUpdate: (details) {
+                setState(() {
+                  _x += details.delta.dx;
+                  _y += details.delta.dy;
+                });
+              },
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Center(
+                  child: Text(
+                    "Drag me!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
                     ),
-                    AnimatedBuilder(
-                      animation: _flipAnimation,
-                      builder: (context, child) {
-                        return Transform(
-                          transform: Matrix4.identity()
-                            ..rotateY(_flipAnimation.value),
-                          alignment: Alignment.centerLeft,
-                          child: ClipPath(
-                            clipper: HalfCircleClipper(CircleSide.right),
-                            child: Container(
-                              height: 100,
-                              width: 100,
-                              color: Colors.green,
-                            ),
-                          ),
-                        );
-                      },
-                    )
-                  ],
+                  ),
                 ),
               ),
-            );
-          },
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
